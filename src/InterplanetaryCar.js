eca4bdf5186aba83ad7af98e1272cd8096990140
Kevin <kevcespedesarancibia87@gmail.com>
2021-10-14 01:36:02 -0400

    Commands with missed /

--- src/InterplanetaryCar.js
+++ src/InterplanetaryCar.js
@@ -1,6 +1,10 @@
 function splitCommandParts(command)
 {
-  return command.split('/')
+  let commandParts = command.split('/');
+  let commandIsComplete = commandParts.length == 3;
+  if (!commandIsComplete)
+    commandParts = ["","",""];
+  return commandParts;
 }
 
 export {splitCommandParts};
--- src/InterplanetaryCar.test.js
+++ src/InterplanetaryCar.test.js
@@ -11,3 +11,9 @@ describe("Split de la cadena de comandos", () => {
     expect(splitCommandParts("5,5//IAIAIAIAA")).toEqual(["5,5","","IAIAIAIAA"]);
   });
 });
+
+describe("Comando con partes faltantes (Falta '/') 5,5/1,2NIAIA ", () => {
+  it("TEST1: Retornar una lista con tres cadenas vacias", () => {
+    expect(splitCommandParts("5,5/1,2NIAIAIAIAA")).toEqual(["","",""]);
+  });
+});
