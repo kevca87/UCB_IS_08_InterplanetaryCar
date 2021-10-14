429d9af4a22b4f7bbc0ad63c916072413673fdb3
Kevin <kevcespedesarancibia87@gmail.com>
2021-10-14 01:24:19 -0400

    Command line split correctly

--- src/InterplanetaryCar.test.js
+++ src/InterplanetaryCar.test.js
@@ -7,4 +7,7 @@ describe("Split de la cadena de comandos", () => {
   it("TEST2: Debe retornar una lista las tres partes de los comandos de entrada", () => {
     expect(splitCommandParts("5,5/1,2N/IAIAA")).toEqual(["5,5","1,2N","IAIAA"]);
   });
+  it("TEST3: Listar las tres partes de los comandos, considerando alguno faltante", () => {
+    expect(splitCommandParts("5,5//IAIAIAIAA")).toEqual(["5,5","","IAIAIAIAA"]);
+  });
 });
